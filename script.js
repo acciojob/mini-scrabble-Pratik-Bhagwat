a4453bc8f710097380d5d84e566df194ddd3756a
Pratik-Bhagwat <126460458+Pratik-Bhagwat@users.noreply.github.com>
2023-03-13 11:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const showLetterCount = document.getElementById("letterCount");
+
+let showTheCount = () => {
+  const getTextLength = document.getElementById("evaluatedText").value;
+  const count = getTextLength.length;
+  showLetterCount.innerHTML = count;
+};
